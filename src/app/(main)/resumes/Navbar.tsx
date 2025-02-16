ffa48dc6738da8dee97b7737275f5f17b52caa3f
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-02-16 12:50:21 +0200

    add navbar

--- src/app/(main)/resumes/Navbar.tsx
+++ src/app/(main)/resumes/Navbar.tsx
@@ -1,1 +1,35 @@
+import Image from "next/image";
+import logo from "@/assets/logo.png";
+import Link from "next/link";
+import { UserButton } from "@clerk/nextjs";
 
+const Navbar = () => {
+  return (
+    <header className="shadow-sm">
+      <div className="mx-auto flex max-w-7xl items-center justify-between gap-3 p-3">
+        <Link href="/resume" className="flex items-center gap-2">
+          <Image
+            src={logo}
+            alt={"logo"}
+            width={35}
+            height={35}
+            className="rounded-full"
+          />
+          <span className="text-xl font-bold tracking-tight">
+            AI Resume Builder
+          </span>
+        </Link>
+        <UserButton appearance={{
+          elements:{
+            avatarBox:{
+              width:35,
+              height:35,
+            }
+          }
+        }} />
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
