2a2d04a14d8447dbe0209c70bd0142d6c8ceb47d
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-02-17 12:45:33 +0200

    add button logic in footer

--- src/app/(main)/editor/Footer.tsx
+++ src/app/(main)/editor/Footer.tsx
@@ -1,28 +1,41 @@
-import Link from "@/components/link"
+import Link from "@/components/link";
 import { Button } from "@/components/ui/button";
+import { steps } from "./steps";
 
-interface FooterProps{
+interface FooterProps {
   currentStep: string;
-  setCurrentStep: (step:string)=>void
+  setCurrentStep: (step: string) => void;
 }
 
-const Footer = ({currentStep,setCurrentStep}:FooterProps) => {
+const Footer = ({ currentStep, setCurrentStep }: FooterProps) => {
+  const previousStep = steps.find(
+    (_, index) => steps[index + 1]?.key === currentStep,
+  )?.key;
+  const nextStep = steps.find(
+    (_, index) => steps[index - 1]?.key === currentStep,
+  )?.key;
   return (
     <footer className="w-full border-t px-3 py-5">
-    <div className="mx-auto flex max-w-7xl flex-wrap justify-between gap-3">
-      <div className="flex items-center gap-3">
-        <Button variant="secondary">Previous step</Button>
-        <Button>Next step</Button>
+      <div className="mx-auto flex max-w-7xl flex-wrap justify-between gap-3">
+        <div className="flex items-center gap-3">
+          <Button variant="secondary"
+          onClick={previousStep ? ()=> setCurrentStep(previousStep) : undefined}
+          disabled={!previousStep}
+          >Previous step</Button>
+          <Button
+          onClick={nextStep ? ()=> setCurrentStep(nextStep) : undefined}
+          disabled={!nextStep}
+          >Next step</Button>
+        </div>
+        <div className="flex items-center gap-3">
+          <Button variant="secondary" asChild>
+            <Link href="/resumes">Close</Link>
+          </Button>
+          <p className="text-muted-foreground opacity-0">Saving...</p>
+        </div>
       </div>
-      <div className="flex items-center gap-3">
-        <Button variant="secondary" asChild>
-          <Link href="/resumes">Close</Link>
-        </Button>
-        <p className="text-muted-foreground opacity-0">Saving...</p>
-      </div>
-    </div>
-  </footer>
-  )
-}
+    </footer>
+  );
+};
 
-export default Footer
+export default Footer;
