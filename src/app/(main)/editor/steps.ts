266bef0cdd0d40296a7e6f6b0e437bcef57b5d56
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-02-17 20:56:45 +0200

    add SummaryForm component and add step for it

--- src/app/(main)/editor/steps.ts
+++ src/app/(main)/editor/steps.ts
@@ -4,6 +4,7 @@ import PersonalInfoForm from "./forms/PersonalInfoForm";
 import WorkExperienceForm from "./forms/WorkExperienceForm";
 import EducationForm from "./forms/EducationForm";
 import SkillsForm from "./forms/SkillsForm";
+import SummaryForm from "./forms/SummaryForm";
 
 export const steps: {
   title: string;
@@ -16,4 +17,5 @@ export const steps: {
   {title:"Work experience",component: WorkExperienceForm,key:"Work-experience"},
   {title:"Education",component: EducationForm,key:"Education"},
   {title:"Skills",component: SkillsForm,key:"Skills"},
+  {title:"Summary",component: SummaryForm,key:"Summary"},
 ];
