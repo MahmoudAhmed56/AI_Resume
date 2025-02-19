555c0cb88a4c1c25f9137c00b79fd8c9e898d9d5
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-02-19 18:35:28 +0200

    add EducationSection cumponent in summary preview

--- src/components/ResumePreview.tsx
+++ src/components/ResumePreview.tsx
@@ -30,6 +30,7 @@ const ResumePreview = ({ resumeData, className }: ResumePreviewProps) => {
         <PersonalInfoHeader resumeData={resumeData} />
         <SummarySection resumeData={resumeData} />
         <ExperienceSection resumeData={resumeData} />
+        <EducationSection resumeData={resumeData} />
       </div>
     </div>
   );
@@ -125,13 +126,46 @@ function ExperienceSection({ resumeData }: ResumeSectionProps) {
           <div key={index} className="break-inside-avoid space-y-1">
             <div className="flex items-center justify-between text-sm font-semibold">
               <span>{exp.position}</span>
-              {exp.startDate && (<span>
-                {formatDate(exp.startDate,"MM/yyyy")} - {" "}
-                {exp.endDate ? formatDate(exp.endDate,"MM/yyyy") : ""}
-              </span>)}
+              {exp.startDate && (
+                <span>
+                  {formatDate(exp.startDate, "MM/yyyy")} -{" "}
+                  {exp.endDate ? formatDate(exp.endDate, "MM/yyyy") : "Present"}
+                </span>
+              )}
             </div>
             <p className="text-xs font-semibold">{exp.company}</p>
             <div className="whitespace-pre-line text-xs">{exp.description}</div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+}
+
+function EducationSection({ resumeData }: ResumeSectionProps) {
+  const { educations } = resumeData;
+  const educationsNotEmpty = educations?.filter(
+    (edu) => Object.values(edu).filter(Boolean).length > 0,
+  );
+
+  if (!educationsNotEmpty?.length) return null;
+  return (
+    <>
+      <hr className="border-2" />
+      <div className="space-y-3">
+        <p className="text-lg font-semibold">Education</p>
+        {educationsNotEmpty.map((edu, index) => (
+          <div key={index} className="break-inside-avoid space-y-1">
+            <div className="flex items-center justify-between text-sm font-semibold">
+              <span>{edu.degree}</span>
+              {edu.startDate && (
+                <span>
+                  {edu.startDate &&
+                    `${formatDate(edu.startDate, "MM/yyyy")} ${edu.endDate ? `- ${formatDate(edu.endDate, "MM/yyyy")}` : ""}`}
+                </span>
+              )}
+            </div>
+            <p className="text-xs font-semibold">{edu.school}</p>
           </div>
         ))}
       </div>
