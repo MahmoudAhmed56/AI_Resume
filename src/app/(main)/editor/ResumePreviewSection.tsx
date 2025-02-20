2803561e64b13bf036f5b1d46e6a8852e98b0b5c
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-02-20 19:23:02 +0200

    add BorderStyleButton component

--- src/app/(main)/editor/ResumePreviewSection.tsx
+++ src/app/(main)/editor/ResumePreviewSection.tsx
@@ -3,6 +3,7 @@ import { ResumeValues } from "@/lib/validation";
 import ColorPicker from "./ColorPicker";
 import { ChangeEvent } from "react";
 import { ColorResult } from "react-color";
+import BorderStyleButton from "./BorderStyleButton";
 
 interface ResumePreviewSectionProps {
   resumeData: ResumeValues;
@@ -14,7 +15,7 @@ const ResumePreviewSection = ({
   setResumeData,
 }: ResumePreviewSectionProps) => {
   return (
-    <div className="relative hidden w-1/2 md:flex">
+    <div className="group relative hidden w-1/2 md:flex">
       <div className="absolute left-1 top-1 flex flex-none flex-col gap-3 opacity-50 transition-opacity group-hover:opacity-100 lg:left-3 lg:top-3 xl:opacity-100">
         <ColorPicker
           color={resumeData.colorHex}
@@ -22,6 +23,7 @@ const ResumePreviewSection = ({
             setResumeData({ ...resumeData, colorHex: color.hex })
           }
         />
+        <BorderStyleButton borderStyle={resumeData.borderStyle} onChange={(borderStyle)=>setResumeData({ ...resumeData, borderStyle: borderStyle })}/>
       </div>
       <div className="flex w-full justify-center overflow-y-auto bg-secondary p-3">
         <ResumePreview
