5e80a78800b39f677964367599b75b73879a3ea4
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-02-22 15:48:03 +0200

    add loading to app

---
+++ src/app/loading.tsx
@@ -0,0 +1,9 @@
+const Loading = () => {
+  return (
+    <div className="flex flex-row space-x-4">
+      <div className="h-12 w-12 animate-spin rounded-full border-4 border-dashed border-green-500 border-t-transparent"></div>
+    </div>
+  );
+};
+
+export default Loading;
