1dcfccb67415c2871aa347465e931b8d6c2729e8
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-02-27 21:36:42 +0200

    merging

--- src/app/[locale]/(main)/editor/ResumePreviewSection.tsx
+++ src/app/[locale]/(main)/editor/ResumePreviewSection.tsx
@@ -3,7 +3,10 @@ import { ResumeValues } from "@/lib/validation";
 import ColorPicker from "./ColorPicker";
 import BorderStyleButton from "./BorderStyleButton";
 import { cn } from "@/lib/utils";
+<<<<<<< HEAD:src/app/[locale]/(main)/editor/ResumePreviewSection.tsx
 
+=======
+>>>>>>> parent of f124edb (fix some ui issue):src/app/(main)/editor/ResumePreviewSection.tsx
 
 interface ResumePreviewSectionProps {
   resumeData: ResumeValues;
