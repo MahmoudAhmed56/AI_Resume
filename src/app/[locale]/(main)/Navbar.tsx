22ce34aef1740839441967f2b736be16e8877330
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-02-28 14:18:13 +0200

    add translation to navbar

--- src/app/[locale]/(main)/Navbar.tsx
+++ src/app/[locale]/(main)/Navbar.tsx
@@ -3,10 +3,14 @@ import logo from "@/assets/logo.png";
 import Link from "next/link";
 import UserButton from "./_components/UserButton";
 import ThemeToggle from "@/components/ThemeToggle";
+import { getCurrentLocale } from "@/lib/getCurrentLocale";
+import getTrans from "@/lib/translation";
 
 
 
-const Navbar = () => {
+const Navbar = async () => {
+  const locale = await getCurrentLocale()
+  const {navLogo} = await getTrans(locale)
   return (
     <header className="shadow-sm">
       <div className="mx-auto flex max-w-7xl items-center justify-between gap-3 p-3">
@@ -19,7 +23,7 @@ const Navbar = () => {
             className="rounded-full"
           />
           <span className="text-xl font-bold tracking-tight">
-            AI Resume Builder
+          {navLogo}
           </span>
         </Link>
         <div className="flex items-center gap-3">
