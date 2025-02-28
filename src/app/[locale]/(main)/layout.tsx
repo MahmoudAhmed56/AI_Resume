14e26388af99657be320e1cc2ee20a149c668a63
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-02-28 13:21:02 +0200

    add getCurrentLocale

--- src/app/[locale]/(main)/layout.tsx
+++ src/app/[locale]/(main)/layout.tsx
@@ -3,7 +3,6 @@ import Navbar from "./Navbar";
 import { auth } from "@clerk/nextjs/server";
 import { getUserSubscriptionLevel } from "@/lib/subscription";
 import SubscriptionLevelProvider from "./SubscriptionLevelProvider";
-import { ClerkProvider } from "@clerk/nextjs";
 
 const Layout = async({
   children,
@@ -17,13 +16,11 @@ const Layout = async({
   const userSubscriptionLevel = await getUserSubscriptionLevel(userId)
   return (
     <SubscriptionLevelProvider userSubscriptionLevel={userSubscriptionLevel}>
-      <ClerkProvider>
     <div className="flex min-h-screen flex-col">
       <Navbar />
       {children}
       <PremiumModal />
     </div>
-    </ClerkProvider>
     </SubscriptionLevelProvider>
   )
 }
