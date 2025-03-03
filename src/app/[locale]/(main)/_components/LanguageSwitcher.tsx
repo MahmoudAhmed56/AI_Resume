28c868bcf80a652fccc9a10bad005f7abc8a33c9
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-03-04 00:08:10 +0200

    fix switch language button

--- src/app/[locale]/(main)/_components/LanguageSwitcher.tsx
+++ src/app/[locale]/(main)/_components/LanguageSwitcher.tsx
@@ -1,16 +1,34 @@
 "use client";
-import { useParams, usePathname, useRouter } from "next/navigation";
+import {
+  useParams,
+  usePathname,
+  useRouter,
+  useSearchParams,
+} from "next/navigation";
 import { Button } from "@/components/ui/button";
 import { Languages } from "@/i18n.config";
 const LanguageSwitcher = () => {
   const router = useRouter();
+
   const pathname = usePathname();
   const { locale } = useParams();
-  
+  const searchParams = useSearchParams();
+  const resumeId = searchParams.get("resumeId"); // Access query parameters
+  console.log(pathname, "pathname");
+  const page = pathname.split("/").pop();
+
   const switchLanguage = (newLocale: string) => {
-    const path =
-      pathname?.replace(`/${locale}`, `/${newLocale}`) ?? `/${newLocale}`;
-    router.push(path);
+    if (!resumeId) {
+      const path =
+        pathname?.replace(`/${locale}`, `/${newLocale}`) ?? `/${newLocale}`;
+      router.push(path);
+    } else {
+      const path = pathname?.replace(
+        `/${locale}/${page}`,
+        `/${newLocale}/${page}/?resumeId=${resumeId}`,
+      );
+      router.push(path);
+    }
   };
   return (
     <div className="flex">
--- src/app/[locale]/(main)/resumes/ResumeItem.tsx
+++ src/app/[locale]/(main)/resumes/ResumeItem.tsx
@@ -113,7 +113,7 @@ function MoreMenu({ resumeId, onPrintClick,translation }: MoreMenuProps) {
           <Button
             variant="ghost"
             size="icon"
-            className="absolute right-0.5 top-0.5 opacity-0 transition-opacity group-hover:opacity-100"
+            className="absolute right-0.5 top-0.5 lg:opacity-0 transition-opacity group-hover:opacity-100"
           >
             <MoreVertical className="size-4" />
           </Button>
