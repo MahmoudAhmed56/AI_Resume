2b8f685c875eacee86c0e8def9aa2065eeb15300
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-03-03 14:46:31 +0200

    add stlyle to languages2

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -58,6 +58,17 @@ export default {
   		}
   	},
 		screens: {
+			'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
 			'sm600': {'max': '600px'},
 			'sm500': {'max': '500px'},
 			'sm400': {'max': '400px'},
