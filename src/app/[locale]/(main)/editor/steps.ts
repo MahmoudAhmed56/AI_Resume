7f55f2a864b88349dc3bbb4423f8c2f2d76705c6
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-03-07 17:28:31 +0200

    add Links step

--- src/app/[locale]/(main)/editor/steps.ts
+++ src/app/[locale]/(main)/editor/steps.ts
@@ -6,6 +6,7 @@ import EducationForm from "./forms/EducationForm";
 import SkillsForm from "./forms/SkillsForm";
 import LanguagesForm from "./forms/LanguagesForm";
 import SummaryForm from "./forms/SummaryForm";
+import SummaryForm from "./forms/LinksForm";
 import ProjectsForm from "./forms/ProjectsForm";
 
 export const steps: {
@@ -21,5 +22,6 @@ export const steps: {
   {title:{English:"Education",Arabic:"التعليم"},component: EducationForm,key:"Education"},
   {title:{English:"Skills",Arabic:"المهارات"},component: SkillsForm,key:"Skills"},
   {title:{English:"Languages",Arabic:"اللغات"},component: LanguagesForm,key:"Languages"},
+  {title:{English:"Links",Arabic:"الروابط"},component: LinksForm,key:"Links"},
   {title:{English:"Summary",Arabic:"الملخص"},component: SummaryForm,key:"Summary"},
 ];
