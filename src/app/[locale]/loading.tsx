bb9cb0d12874caf3ce1ec24b5e35f3db16e23cd4
Mahmoud Ahmed <mahmoudcs123@gmail.com>
2025-03-10 17:51:21 +0200

    update loader

--- src/app/[locale]/loading.tsx
+++ src/app/[locale]/loading.tsx
@@ -1,8 +1,7 @@
+import styles from './loading.module.css'; 
 const Loading = () => {
   return (
-    <div className="flex flex-row space-x-4">
-      <div className="h-12 w-12 animate-spin rounded-full border-4 border-dashed border-green-500 border-t-transparent"></div>
-    </div>
+      <span className={styles.loader}></span>
   );
 };
 
